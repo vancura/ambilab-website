5ec86e8b94d86fe10dda4cb83269f978c8204332
Vaclav Vancura <commit@vancura.dev>
2026-02-07 16:32:59 +0100

    chore(content): remove obsolete files
    Signed-off-by: Vaclav Vancura <commit@vancura.dev>

--- src/pages/cs/rss.xml.ts
+++
@@ -1,6 +0,0 @@
-import { generateRssFeed } from '@utils/rss';
-import type { APIContext } from 'astro';
-
-export async function GET(context: APIContext) {
-    return generateRssFeed(context, 'cs', 'Čeština', 'cs-CZ');
-}
